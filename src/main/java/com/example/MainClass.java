0db92d2ea5331e2c09b9e73ffa389ca3306646a4
VarshiniSmitha <varshinismitha2005@gmail.com>
2026-02-23 09:43:46 +0530

    MainClass.java

---
+++ src/main/java/com/example/MainClass.java
@@ -0,0 +1,14 @@
+package com.example;
+
+
+public class MainClass
+{
+public static void main(String args[])
+{
+int a=10,b=20;
+int sum=0;
+sum=a+b;
+System.out.println("Sum of 2 numbers is"+sum);
+}
+}
+
